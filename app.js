00e6618788ee1361fc801e92abd11a3b52d28883
Daniele Maselli <danymase@gmail.com>
2024-01-17 17:11:43 +0100

    added code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
+const Recipe = require('./models/Recipe.model')
 const app = express();
 
 // MIDDLEWARE
@@ -11,8 +12,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
-
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -20,17 +25,47 @@ app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
-
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post('/recipes', (res,req) => {
+    
+    Recipe.create({
+        title: req.body.title,
+        level: req.body.level,
+        ingredients: req.body.level,
+        cuisine: req.body.cuisine,
+        dishType: req.body.dishType,
+        image: req.body.image,
+        duration: req.body.duration,
+        creator: req.body.creator
+    })
+    .then((createdRecipe)=> {
+        res.status(201).json(createdRecipe)
+    })
+    .catch((error)=> {
+        res.status(500).json({message: 'internal server error'})
+    })
+    
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get('/recipes',(res,req) => {
 
+    Recipe.find()
+        .then((allRecipes)=> {
+            res.status(200).json(allRecipes)
+        })
+        .catch((error)=> {
+            res.status(500).json({message: "error"})
+        })
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get('/',(res,req) => {
+    
+})
 
 
 //  Iteration 6 - Update a Single Recipe
@@ -40,10 +75,8 @@ app.get('/', (req, res) => {
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(5000, () => console.log('My first app listening on port 3000!'));
 
 
 
